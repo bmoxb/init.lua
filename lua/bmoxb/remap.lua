4951e27edbb02e5ab8798a014c99e314b1f7f830
bmoxb <maxoblack@yahoo.com>
2024-07-04 12:57:04 +0900

    Add mapping to rename a symbol.

--- lua/bmoxb/remap.lua
+++ lua/bmoxb/remap.lua
@@ -1,4 +1,11 @@
+-- paste without overwriting default register
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- yank to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+-- yank line to system clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- rename a symbol
+vim.keymap.set({ "n", "v" }, "<leader>rn", vim.lsp.buf.rename)
